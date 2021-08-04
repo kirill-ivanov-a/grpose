e60aff204244e9fd3bf3a59c3a1cd2f4771bfe95
Mikhail Terekhov <Mikhail.A.Terekhov@ya.ru>
2021-08-04 18:05:30 +0200

    Fix type_traits on clang

--- include/util/type_traits.h
+++ include/util/type_traits.h
@@ -1,19 +1,30 @@
 #ifndef GRPOSE_UTIL_TYPE_TRAITS_
 #define GRPOSE_UTIL_TYPE_TRAITS_
 
+#include <type_traits>
+
 namespace grpose {
 
 template <typename EigenType>
-constexpr bool IsStatic(const Eigen::MatrixBase<EigenType> &object);
+constexpr bool kIsStatic =
+    (std::remove_reference_t<EigenType>::SizeAtCompileTime != Eigen::Dynamic);
 
 template <typename EigenType>
-constexpr bool IsVector2Static(const Eigen::MatrixBase<EigenType> &object);
+constexpr bool kIsVector2Static =
+    (std::is_same_v<typename std::remove_reference_t<EigenType>::Scalar,
+                    double> &&
+     std::remove_reference_t<EigenType>::RowsAtCompileTime == 2 &&
+     std::remove_reference_t<EigenType>::ColsAtCompileTime == 1);
+
+template <typename EigenType>
+constexpr bool kIsVector3Static =
+    (std::is_same_v<typename std::remove_reference_t<EigenType>::Scalar,
+                    double> &&
+     std::remove_reference_t<EigenType>::RowsAtCompileTime == 3 &&
+     std::remove_reference_t<EigenType>::ColsAtCompileTime == 1);
 
 template <typename EigenType>
 inline bool IsVector2Dynamic(const Eigen::MatrixBase<EigenType> &object);
-
-template <typename EigenType>
-constexpr bool IsVector3Static(const Eigen::MatrixBase<EigenType> &object);
 
 template <typename EigenType>
 inline bool IsVector3Dynamic(const Eigen::MatrixBase<EigenType> &object);
@@ -21,29 +32,9 @@ inline bool IsVector3Dynamic(const Eigen::MatrixBase<EigenType> &object);
 // Implementation
 
 template <typename EigenType>
-inline bool IsVector3Dynamic(const Eigen::MatrixBase<EigenType> &object);
-
-template <typename EigenType>
-constexpr bool IsStatic(const Eigen::MatrixBase<EigenType> &object) {
-  return EigenType::SizeAtCompileTime != Eigen::Dynamic;
-}
-
-template <typename EigenType>
-constexpr bool IsVector2Static(const Eigen::MatrixBase<EigenType> &object) {
-  return std::is_same_v<typename EigenType::Scalar, double> &&
-         EigenType::RowsAtCompileTime == 2 && EigenType::ColsAtCompileTime == 1;
-}
-
-template <typename EigenType>
 inline bool IsVector2Dynamic(const Eigen::MatrixBase<EigenType> &object) {
   return std::is_same_v<EigenType, double> && object.rows() == 2 &&
          object.cols() == 1;
-}
-
-template <typename EigenType>
-constexpr bool IsVector3Static(const Eigen::MatrixBase<EigenType> &object) {
-  return std::is_same_v<typename EigenType::Scalar, double> &&
-         EigenType::RowsAtCompileTime == 3 && EigenType::ColsAtCompileTime == 1;
 }
 
 template <typename EigenType>
@@ -56,8 +47,8 @@ inline bool IsVector3Dynamic(const Eigen::MatrixBase<EigenType> &object) {
 
 #define GRPOSE_CHECK_IS_VECTOR2(object)                              \
   {                                                                  \
-    if constexpr (IsStatic((object)))                                \
-      static_assert(IsVector2Static((object)),                       \
+    if constexpr (kIsStatic<decltype((object))>)                     \
+      static_assert(kIsVector2Static<decltype((object))>,            \
                     "Expected Vector2-sized object!");               \
     else if (!IsVector2Dynamic((object)))                            \
       throw std::invalid_argument("Expected Vector2-sized object!"); \
@@ -65,8 +56,8 @@ inline bool IsVector3Dynamic(const Eigen::MatrixBase<EigenType> &object) {
 
 #define GRPOSE_CHECK_IS_VECTOR3(object)                              \
   {                                                                  \
-    if constexpr (IsStatic((object)))                                \
-      static_assert(IsVector3Static((object)),                       \
+    if constexpr (kIsStatic<decltype((object))>)                     \
+      static_assert(kIsVector3Static<decltype((object))>,            \
                     "Expected Vector3-sized object!");               \
     else if (!IsVector3Dynamic((object)))                            \
       throw std::invalid_argument("Expected Vector3-sized object!"); \
