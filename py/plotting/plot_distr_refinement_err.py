855eb68f1e817c791887ad449e3554cf81c53227
Mikhail Terekhov <Mikhail.A.Terekhov@ya.ru>
2021-11-12 12:38:38 +0100

    Enhance the error plotting script

--- py/plotting/plot_distr_refinement_err.py
+++ py/plotting/plot_distr_refinement_err.py
@@ -9,7 +9,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument('table')
 parser.add_argument('--crop_val', type=float, nargs='?', default=100)
 parser.add_argument('--types', type=str, nargs='+', 
-                    default=['min_solver','ba','reproj_2d','sampson_3d','dot_prod'])
+                    default=['min_solver','reproj_2d','sampson_3d','sampson_2d',
+                             'dot_prod','sampson_pinhole','algebraic',
+                             'symmetric_epipolar_pinhole',
+                             'symmetric_epipolar_cosine'])
+parser.add_argument('--plot_iter', action='store_true')
+parser.add_argument('--ecdf', action='store_true')
 args = parser.parse_args()
 
 data = pd.read_csv(args.table)
@@ -22,7 +27,23 @@ print(f'Retained {cropped} out of {total}')
 
 data = data[data['type'].isin(args.types)]
 
-fig, ax = plt.subplots(1, 2)
-for i, error in enumerate(['are', 'rte']):
-    sns.histplot(data, x=error, hue="type", ax=ax[i])
+medians = data.groupby('type')['rte','are'].median()
+print(f'medians:\n{medians}')
+averages = data.groupby('type')['rte','are'].mean()
+print(f'averages:\n{averages}')
+stds = data.groupby('type')['rte','are'].std()
+print(f'stds:\n{stds}')
+
+if args.plot_iter:
+    if args.ecdf:
+        sns.ecdfplot(data, x='num_iter', hue='type')
+    else:
+        sns.histplot(data, x='num_iter', hue='type')
+else:
+    fig, ax = plt.subplots(1, 2)
+    for i, error in enumerate(['are', 'rte']):
+        if args.ecdf:
+            sns.ecdfplot(data, x=error, hue='type', ax=ax[i])
+        else:
+            sns.histplot(data, x=error, hue="type", ax=ax[i])
 plt.show()
